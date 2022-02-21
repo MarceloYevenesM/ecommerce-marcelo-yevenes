ad3372d63f7dd33af121d3ae204bc5acbf85104c
MarceloYevenesM <marcelo.yevenes.moreno@gmail.com>
2022-02-21 19:01:30 -0300

    feat:Added the title under the appBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Bienvenid@ a Papeleria Galaktika"/>
     </div>
   );
 }
